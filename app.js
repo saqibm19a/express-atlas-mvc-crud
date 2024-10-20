a5ea09432bb4581c19ef95b017356e4ba1021b65
Muhammad Saqib <sm4179515@gmail.com>
2024-10-20 14:10:57 +0500

    now some changeing in the app file

--- app.js
+++ app.js
@@ -11,12 +11,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Connect to MongoDB
-connectDB();
-
-// Routes
-app.use('/api', userRoutes);
-
 // Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
