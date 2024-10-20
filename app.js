4e1a0a7864e4a8c20c02d04c6884c6a18d2581e5
Muhammad Saqib <sm4179515@gmail.com>
2024-10-20 14:11:43 +0500

    connect to DB file to app

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Connect to MongoDB
+connectDB();
+
+
 // Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
