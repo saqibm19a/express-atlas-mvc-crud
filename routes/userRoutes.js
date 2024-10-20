0de250b3f93181a57662eb04ea7c02c851e53138
Muhammad Saqib <sm4179515@gmail.com>
2024-10-20 14:01:35 +0500

    Create User Done

---
+++ routes/userRoutes.js
@@ -0,0 +1,13 @@
+import User from '../models/User.js';
+
+// Create User
+export const createUser = async (req, res) => {
+    try {
+        const { name, email } = req.body;
+        const newUser = new User({ name, email });
+        await newUser.save();
+        res.status(201).json(newUser);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
