1a82ce487bc1ca9ae64be5d1ae3a12f949d80d75
Muhammad Saqib <sm4179515@gmail.com>
2024-10-20 14:03:10 +0500

    Get Single User Done

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -21,3 +21,15 @@ export const getUsers = async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 };
+
+// Get Single User
+export const getUser = async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        if (!user) return res.status(404).json({ message: 'User not found' });
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
