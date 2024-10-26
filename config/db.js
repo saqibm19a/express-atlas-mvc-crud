db1e7c58cac3f73d4215e10760e8da3bffd1b4a1
Muhammad Saqib <sm4179515@gmail.com>
2024-10-26 15:05:31 +0500

    Update the connected to DB file to app

--- config/db.js
+++ config/db.js
@@ -5,15 +5,13 @@ dotenv.config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log('MongoDB connected');
     } catch (err) {
         console.error('MongoDB connection error', err.message);
         process.exit(1);
     }
 };
+
 
 export default connectDB;
